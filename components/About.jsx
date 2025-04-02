dab3bd12359536ecb2ebc665bc34b3ed0d317f4b
Diego Vilhalva <diegorocha450@yahoo.com>
2025-04-02 09:04:18 -0300

    about section finished

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,34 @@
+import Image from "next/image"
+import PreTitle from "./PreTitle"
+import Button from "./Button"
 
 
 const About = () => {
   return (
-    <div id="about">About</div>
+    <div id="about" className="pt-16 xl:pt-32">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-12 xl:gap-0 xl:flex-row xl:items-center">
+          <div className="flex-1 ">
+            <div className="max-w-[540px]">
+              <PreTitle text="About us"  />
+              <h2 className="h2 mb-6">Focused on Excellence in Every Project</h2>
+              <p className="mb-11">Our unwearing commitment to excellence drives  every project we undertake. From concept to completion, we meticulously craft solutions that embody quality, precision and innovation.</p>
+              <div className="w-max flex flex-col text-right mb-10">
+                <Image src="/assets/img/about/signature.svg" width={154} height={38} alt="" />
+                <p>Company CEO</p>
+              </div>
+              <Button text="Contact us" />
+            </div>
+          </div>
+          <div className="flex-1 xl:flex xl:justify-center">
+            <div className="xl:w-[444px] xl:h-[493px] relative">
+              <div className="hidden xl:flex w-[444px] h-[493px] bg-accent absolute -top-4 -left-4 -z-10"></div>
+              <Image src={"/assets/img/about/img.jpg"} width={444} height={492} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
